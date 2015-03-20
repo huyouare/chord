36c0f5b57531ff7e749956a055aa7505932f3478
Jesse Hu <huyouare@gmail.com>
2015-03-19 20:51:19 -0400

    Implement key hashing

--- chord.c
+++ chord.c
@@ -8,33 +8,51 @@
 #include <pthread.h>
 #include <openssl/sha.h>
 
+
 #define   FILTER_FILE   "chord.filter"
 #define   LOG_FILE      "chord.log"
 #define   DEBUG_FILE    "chord.debug"
+#define   MAX_RING_SIZE 100
 
+ typedef struct Node {
+  char *ip_address;
+  char *port;
+  char *id;
+  int key;
+  struct Node *predecessor;
+  struct Node *successor;
+  int *table;
+  // struct Table *table;
+} Node;
+
+typedef struct ChordRing {
+  int size;
+  Node *first;
+  Node *last;
+  Node *node_array[MAX_RING_SIZE];
+} ChordRing;
 
 /*============================================================
  * function declarations
  *============================================================*/
 
-void initializeChord(int port);
-void addNewNode(char *ipAddress, char *port);
+void initialize_chord(int port);
+void add_new_node(char *ip_address, char *port);
+void receive_client(void *args);
 
 int main(int argc, char *argv[])
 { 
-  int chordPort;
+  int chord_port;
   unsigned char hash[SHA_DIGEST_LENGTH];
 
   if (argc == 2) {
-    chordPort = atoi(argv[1]);
-    printf("Creating new Chord ring...\n");
-    initializeChord(chordPort);
-    printf("Listening on port %d\n", chordPort);
+    chord_port = atoi(argv[1]);
+    initialize_chord(chord_port);
   } else if (argc == 4) {
-    chordPort = atoi(argv[1]);
+    chord_port = atoi(argv[1]);
     printf("Joining the Chord ring...\n");
-    addNewNode(argv[2], argv[3]);
-    printf("You are listening on port %d\n", chordPort);
+    add_new_node(argv[2], argv[3]);
+    printf("You are listening on port %d\n", chord_port);
     printf("Your position is...\n");
   } else {
     printf("Usage: %s port [nodeAddress nodePort]\n", argv[0]);
@@ -42,23 +60,63 @@ int main(int argc, char *argv[])
   }
 }
 
-void initializeChord(int port) {
+void initialize_chord(int port) {
+  printf("Creating new Chord ring...\n");
+  int listenfd, connfd, optval, clientlen;
+  struct sockaddr_in clientaddr;
+
+  listenfd = Open_listenfd(port);
+  optval = 1;
+  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void*)&optval, sizeof(int));
+  printf("Listening on port %d\n", port);
+
+  while(1) {
+
+    printf("New client\n");
+    clientlen = sizeof(clientaddr); //struct sockaddr_in
+
+    /* accept a new connection from a client here */
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    printf("Connected to client\n");
+    // printf("Addr: %d", clientaddr)
+    // serverPort = 80; // Default port
+
+    pthread_t thread;
+
+    int *args = malloc(2 * sizeof(int));
+    args[0] = connfd;
+    args[1] = port;
+
+    if (pthread_create(&thread, NULL, &receive_client, args) < 0) {
+      printf("receive_client thread error\n");
+    }
+    // pthread_join(&thread);
+
+    printf("Done Thread\n");
+  }
+}
+
+void receive_client(void *args) {
 
 }
 
-void addNewNode(char *ipAddress, char *port) {
-  char hashData[strlen(ipAddress) + strlen(port) + 1];
-  hashData[0] = 0;
+void add_new_node(char *ip_address, char *port) {
+  char data[strlen(ip_address) + strlen(port) + sizeof(char)];
+  data[0] = 0;
   unsigned char hash[SHA_DIGEST_LENGTH];
-  printf("Hashdata: %s\n", hashData);
-  printf("%s\n", ipAddress);
-  strcat(hashData, ipAddress);
-  strcat(hashData, ":");
-  strcat(hashData, port);
-  SHA1(hashData, sizeof(hashData), hash);
-  printf("%s\n", hashData);
+  strcat(data, ip_address);
+  strcat(data, ":");
+  strcat(data, port);
+  SHA1(data, sizeof(data), hash);
+  printf("%s\n", data);
   printf("%s\n", hash);
 
-  
+  uint32_t key;
+  memcpy(&key, &hash, sizeof(key));
+  printf("%d\n", key);
+}
+
+int find_successor(int id, int *table) {
+
 }
 
