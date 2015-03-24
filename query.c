d1470036d5147e4614560f7f0f6081f7bf777d54
Jesse Hu <huyouare@gmail.com>
2015-03-23 20:40:32 -0400

    Finish query options

--- query.c
+++ query.c
@@ -36,6 +36,21 @@ typedef struct Node
 
 void initialize_query(char *ip_address, int port);
 void send_query(char search_key[], char *ip_address, int port);
+void handle_options(char *ip_address, int port, char *option);
+
+Node fetch_query(Node n, char message[]);
+void send_request(Node n, char message[]);
+
+/* Remote functions */
+Node fetch_successor(Node n);
+Node fetch_predecessor(Node n);
+Node query_successor(uint32_t key, Node n);
+Node query_predecessor(uint32_t key, Node n);
+Node query_closest_preceding_finger(uint32_t key, Node n);
+void request_update_successor(Node successor, Node n);
+void request_update_predecessor(Node predecessor, Node n);
+void request_update_finger_table(Node s, int i, Node n);
+Node parse_incoming_node(rio_t *client);
 
 /* Utility functions */
 uint32_t hash_address(char *ip_address, int port);
@@ -50,8 +65,12 @@ int main(int argc, char *argv[])
   if (argc == 3) {
     listen_port = atoi(argv[2]);
     initialize_query(argv[1], listen_port);
-  } else {
-    printf("Usage: %s ip_address port\n", argv[0]);
+  } else if (argc == 4) {
+    listen_port = atoi(argv[2]);
+    handle_options(argv[1], listen_port, argv[3]);
+  }
+  else {
+    printf("Usage: %s ip_address port [options]\n", argv[0]);
     exit(1);
   }
 }
@@ -75,6 +94,30 @@ void initialize_query(char *ip_address, int port) {
     }
 
     send_query(search_key, ip_address, port);
+  }
+}
+
+void handle_options(char *ip_address, int port, char *option) {
+  Node return_node;
+  uint32_t key, hash_value;
+  char search_key[MAXLINE];
+
+  key = hash_address(ip_address, port);
+  Node n;
+  strcpy(n.ip_address, ip_address);
+  n.port = port;
+  n.key = key;
+
+  if (strncmp(option, "fetch_suc", 9) == 0) {
+    return_node = fetch_successor(n);
+    print_node(return_node);
+  }
+  if (strncmp(option, "fetch_pre", 9) == 0) {
+    return_node = fetch_predecessor(n);
+    print_node(return_node);
+  }
+  if (strncmp(option, "print_table", 11) == 0) {
+    send_request(n, option);
   }
 }
 
@@ -119,6 +162,36 @@ void send_query(char search_key[], char *ip_address, int port) {
   Close(sock);
 }
 
+Node parse_incoming_node(rio_t *client) {
+  int numBytes;
+  char request[MAXLINE];
+  Node n;
+
+  numBytes = Rio_readlineb(client, request, MAXLINE);
+  if (numBytes <= 0) {
+    printf("No request received\n");
+  } else {
+    n.key = (uint32_t) atoi(request);
+  }
+  request[0] = 0;
+  numBytes = Rio_readlineb(client, request, MAXLINE);
+  if (numBytes <= 0) {
+    printf("No request received\n");
+  } else {
+    int len = strlen(request);
+    if (len > 0 && request[len-1] == '\n') request[len-1] = '\0';
+    strcpy(n.ip_address, request);
+  }
+  request[0] = 0;
+  numBytes = Rio_readlineb(client, request, MAXLINE);
+  if (numBytes <= 0) {
+    printf("No request received\n");
+  } else {
+    n.port = atoi(request);
+  }
+
+  return n;
+}
 
 uint32_t hash_address(char *ip_address, int port) {
   char port_str[5];
@@ -137,8 +210,194 @@ uint32_t hash_address(char *ip_address, int port) {
   return key;
 }
 
+bool is_between(uint32_t key, uint32_t a, uint32_t b) {
+  if (a < key) {
+    if (b < a) { // wrap around
+      return true;
+    } else {
+      if (key < b) {
+        return true;
+      }
+    }
+  } else {
+    if (key < b && b < a) {
+      return true;
+    }
+  }
+  return false;
+}
+
+Node fetch_successor(Node n) {
+  char request_string[MAXLINE];
+  request_string[0] = 0;
+  strcat(request_string, "fetch_suc");
+  return fetch_query(n, request_string);
+}
+
+Node fetch_predecessor(Node n) {
+  char request_string[MAXLINE];
+  request_string[0] = 0;
+  strcat(request_string, "fetch_pre");
+  return fetch_query(n, request_string);
+}
+
+Node query_predecessor(uint32_t key, Node n) {
+  char request_string[MAXLINE];
+  request_string[0] = 0;
+  strcat(request_string, "query_pre");
+  sprintf(request_string+9, "%u", key);
+  return fetch_query(n, request_string);
+}
+
+Node query_successor(uint32_t key, Node n) {
+  char request_string[MAXLINE];
+  request_string[0] = 0;
+  strcat(request_string, "query_suc");
+  sprintf(request_string+9, "%u", key);
+  return fetch_query(n, request_string);
+}
+
+Node query_closest_preceding_finger(uint32_t key, Node n) {
+  char request_string[MAXLINE];
+  request_string[0] = 0;
+  strcat(request_string, "query_cpf");
+  sprintf(request_string+9, "%u", key);
+  return fetch_query(n, request_string);
+}
+
+void request_update_successor(Node successor, Node n) {
+  char request_string[MAXLINE], buf1[MAXLINE];
+  request_string[0] = 0;
+  strcat(request_string, "update_suc\n");
+  sprintf(buf1, "%u\n", successor.key);
+  strcat(request_string, buf1);
+  strcat(request_string, successor.ip_address);
+  strcat(request_string, "\n");
+  sprintf(buf1, "%d\n", successor.port);
+  strcat(request_string, buf1);
+
+  send_request(n, request_string);
+}
+
+void request_update_predecessor(Node predecessor, Node n) {
+  char request_string[MAXLINE], buf1[MAXLINE];
+  request_string[0] = 0;
+  strcat(request_string, "update_pre\n");
+  sprintf(buf1, "%u\n", predecessor.key);
+  strcat(request_string, buf1);
+  strcat(request_string, predecessor.ip_address);
+  strcat(request_string, "\n");
+  sprintf(buf1, "%d\n", predecessor.port);
+  strcat(request_string, buf1);
+
+  send_request(n, request_string);
+}
+
+void request_update_finger_table(Node s, int i, Node n) {
+  char request_string[MAXLINE], buf1[MAXLINE];
+  request_string[0] = 0;
+  strcat(request_string, "update_fin\n");
+  sprintf(buf1, "%u\n", s.key);
+  strcat(request_string, buf1);
+  strcat(request_string, s.ip_address);
+  strcat(request_string, "\n");
+  sprintf(buf1, "%d\n", s.port);
+  strcat(request_string, buf1);
+  sprintf(buf1, "%d\n", i);
+  strcat(request_string, buf1);
+
+  send_request(n, request_string);
+}
+
+Node fetch_query(Node n, char message[]) {
+  Node return_node;
+  int sock;
+  struct sockaddr_in server_addr;
+  rio_t server;
+
+  if ((sock = socket(AF_INET, SOCK_STREAM/* use tcp */, 0)) < 0) {
+    perror("Create socket error:");
+  }
+
+  server_addr.sin_addr.s_addr = inet_addr(n.ip_address);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(n.port);
+
+  if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    perror("Connect error:");
+  }
+  printf("Connected to server %s:%d\n", n.ip_address, n.port);
+
+  int numBytes;
+  char response[MAXLINE];
+  printf("Message: %s\n", message);
+
+  if (send(sock, message, MAXLINE,0) < 0) {
+    perror("Send error:");
+  }
+  shutdown(sock, SHUT_WR);
+
+  Rio_readinitb(&server, sock);
+  numBytes = Rio_readlineb(&server, response, MAXLINE);
+  if (numBytes <= 0) {
+    printf("No response received\n");
+  } else {
+    return_node.key = (uint32_t) atoi(response);
+  }
+
+  response[0] = 0;
+  numBytes = Rio_readlineb(&server, response, MAXLINE);
+  if (numBytes <= 0) {
+    printf("No response received\n");
+  } else {
+    int len = strlen(response);
+    if (len > 0 && response[len-1] == '\n') response[len-1] = '\0';
+    strcpy(return_node.ip_address, response);
+  }
+
+  response[0] = 0;
+  numBytes = Rio_readlineb(&server, response, MAXLINE);
+  if (numBytes <= 0) {
+    printf("No response received\n");
+  } else {
+    return_node.port = atoi(response);
+  }
+  Close(sock);
+
+  return return_node;
+}
+
+void send_request(Node n, char message[]) {
+  Node return_node;
+  int sock;
+  struct sockaddr_in server_addr;
+  rio_t server;
+
+  if ((sock = socket(AF_INET, SOCK_STREAM/* use tcp */, 0)) < 0) {
+    perror("Create socket error:");
+  }
+
+  server_addr.sin_addr.s_addr = inet_addr(n.ip_address);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(n.port);
+
+  if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    perror("Connect error:");
+  }
+  printf("Connected to server %s:%d\n", n.ip_address, n.port);
+
+  int numBytes;
+  char response[MAXLINE];
+  printf("Message: %s\n", message);
+
+  if (send(sock, message, MAXLINE,0) < 0) {
+    perror("Send error:");
+  }
+  shutdown(sock, SHUT_WR);
+}
+
 void print_node(Node n) {
-  printf("Key: %u\n", n.key);
+  printf("Key: %x\n", n.key);
   printf("IP: %s\n", n.ip_address);
   printf("port: %d\n", n.port);
 }
